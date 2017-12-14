68b2ded23daf420f5f77c1b6dad312c2e9e7ca56
joylty94 <wowmen1757@gmail.com>
2017-12-14 11:56:09 +0900

    타입 변경

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -2,31 +2,31 @@ import { ROWS, COLS } from './config';
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [{ x: 1, y: 0 }, { x: 0, y: 0 }];
+  this.joints = [{ x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
 
   // 먹이의 좌표
-  this.fruit = { x: 5, y: 10 };
+  this.fruit = { x: 3, y: 5 };
 }
 
 SnakeGameLogic.prototype.up = function() {
-  up()
-    // 위쪽 화살표 키를 누르면 실행되는
+  // 위쪽 화살표 키를 누르면 실행되는
   console.log('up');
 }
 SnakeGameLogic.prototype.down = function() {
-  down()
-    // 위쪽 화살표 키를 누르면 실행되는
+  // 위쪽 화살표 키를 누르면 실행되는
   console.log('down');
 }
 SnakeGameLogic.prototype.left = function() {
-  left()
-    // 위쪽 화살표 키를 누르면 실행되는
+  // 위쪽 화살표 키를 누르면 실행되는
   console.log('left');
 }
 SnakeGameLogic.prototype.right = function() {
-  right()
-    // 위쪽 화살표 키를 누르면 실행되는
+  // 위쪽 화살표 키를 누르면 실행되는
   console.log('right');
 }
 
+SnakeGameLogic.prototype.nextState = function() {
+  console.log('nextState');
+  return true;
+}
 export default SnakeGameLogic;
